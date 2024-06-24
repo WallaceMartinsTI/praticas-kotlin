fe46e70a6bf37840893a0527279daeca80855a2e
Wallace Martins <wallace159santos@hotmail.com>
2024-06-24 08:30:56 -0300

    adicionado acerte o numero

---
+++ src/main/kotlin/desafios/basicos/acerte_o_numero.kt
@@ -0,0 +1,26 @@
+package desafios.intermediario
+
+import kotlin.random.Random
+
+fun main() {
+    println("Adivinhe o número de 1 a 10")
+    print("Palpite: ")
+    val input = readln()
+    try {
+        val palpite = input.toInt()
+        val randomNumber = getRandomNumber()
+        if(palpite == randomNumber) {
+			println("Você acertou!")
+        } else {
+            println("Você errou!")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("VALOR INVÁLIDO")
+    }
+  
+}
+
+fun getRandomNumber(): Int {
+    return Random.nextInt(1, 10)
+}
