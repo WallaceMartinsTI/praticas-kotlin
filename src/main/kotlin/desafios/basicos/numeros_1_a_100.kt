21f3eadaa2974d5e775e2702efcf8bc0ed563b56
Wallace Martins <wallace159santos@hotmail.com>
2024-06-27 13:28:50 -0300

    desafios java adicionados

--- src/main/kotlin/desafios/basicos/numeros_1_a_100.kt
+++
@@ -1,9 +0,0 @@
-package desafios.basicos
-
-// Implemente um programa que imprima os números de 1 a 100.
-
-fun main() {
-    for (numero in 1..100) {
-        println(numero)
-    }
-}
--- src/main/kotlin/desafios/basicos/ola_mundo.kt
+++
@@ -1,7 +0,0 @@
-package desafios.basicos
-
-// Escreva um programa em Kotlin que imprima "Olá, mundo!" na tela.
-
-fun main() {
-    println("Olá, mundo!")
-}
