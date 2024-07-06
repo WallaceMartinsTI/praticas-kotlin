03d02eec55a25b41982c84a23b565401ffd15cf2
Wallace Martins <wallace159santos@hotmail.com>
2024-07-05 22:12:37 -0300

    java exercise added

---
+++ desafiosjava/src/main/java/com/wcsm/desafiosjava/basicos/IdentificandoDDD.java
@@ -0,0 +1,56 @@
+package com.wcsm.desafiosjava.basicos;
+
+import java.util.Scanner;
+
+/*
+Leia um número inteiro que representa um código de DDD para discagem interurbana.
+Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+--------------------------------
+| DDD  | Destination           |
+| 61   | Brasilia              |
+| 71   | Salvador              |
+| 11   | São Paulo             |
+| 21   | Rio de Janeiro        |
+| 32   | Juiz de Fora          |
+| 19   | Campinas              |
+| 27   | Vitoria               |
+| 31   | Belo Horizonte        |
+--------------------------------
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima,
+o programa deverá informar:
+
+DDD nao cadastrado
+
+Entrada
+A entrada consiste de um único valor inteiro.
+
+Saída
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.
+
+-----------------------------------------
+| Exemplo de Entrada | Exemplo de Saída |
+| 11                 | São Paulo        |
+-----------------------------------------
+*/
+class IdentificandoDDD {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Informe o DDD: ");
+        int dDD = leitor.nextInt();
+        switch (dDD) {
+            case 61: System.out.println("Brasilia"); break;
+            case 71: System.out.println("Salvador"); break;
+            case 11: System.out.println("Sao Paulo"); break;
+            case 21: System.out.println("Rio de Janeiro"); break;
+            case 32: System.out.println("Juiz de Fora"); break;
+            case 19: System.out.println("Campinas"); break;
+            case 27: System.out.println("Vitoria"); break;
+            case 31: System.out.println("Belo Horizonte"); break;
+            default: System.out.println("DDD nao cadastrado");
+        }
+        leitor.close();
+    }
+
+}
